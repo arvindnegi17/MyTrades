c1ba8e4142bb18345fd641d8f492b067a9e5898b
arvindnegi17 <arvind.negi@tatrasdata.com>
2017-08-31 01:14:33 +0530

    first commit

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controller;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of IndexController
+ *
+ * @author jarvis
+ */
+class IndexController extends AppController{
+    public function index(){
+        exit("welcom to stock market");
+    }
+    //put your code here
+}
